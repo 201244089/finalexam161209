95f82c11b032f1128069a23434574ad6ebec5c54
201344085_LyuSeongMin <j1344085@naver.com>
2016-12-09 18:32:52 +0900

    5. dindViewById 선언 완료

--- app/src/main/java/com/example/ryun/finalexam161209/MainActivity.java
+++ app/src/main/java/com/example/ryun/finalexam161209/MainActivity.java
@@ -30,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
     RadioButton rb1, rb4;
     ImageView img;
     TextView total, saleprice, totalprice;
-    FrameLayout frLay1, frLay2, frLay3;
-    LinearLayout page1, page2;
+    FrameLayout frLay1, frLay2;
+    LinearLayout linLay1, linLay2;
     int year=-1;
     int mon=-1;
     int day=-1;
@@ -43,5 +43,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.button);
+        btn2 = (Button)findViewById(R.id.button2);
+        btn3 = (Button)findViewById(R.id.button3);
+        btn4 = (Button)findViewById(R.id.button4);
+        start_Swi = (Switch)findViewById(R.id.switch1);
+        cronoM = (Chronometer)findViewById(R.id.chronometer);
+        calV = (CalendarView)findViewById(R.id.calendarView);
+        timeP = (TimePicker)findViewById(R.id.timePicker);
+        adult = (EditText) findViewById(R.id.editText1);
+        child = (EditText) findViewById(R.id.editText2);
+        kid = (EditText) findViewById(R.id.editText3);
+        radioG1 = (RadioGroup) findViewById(R.id.rgroup);
+        radioG2 = (RadioGroup) findViewById(R.id.rgroup2);
+        total = (TextView) findViewById(R.id.textView5);
+        saleprice = (TextView) findViewById(R.id.textView6);
+        totalprice = (TextView) findViewById(R.id.textView7);
+        frLay1 = (FrameLayout) findViewById(R.id.frameLayout1);
+        frLay2 = (FrameLayout)findViewById(R.id.framelayout2);
+        img = (ImageView)findViewById(R.id.imageView);
+        linLay1 = (LinearLayout)findViewById(R.id.switchlayout);
+        linLay2 = (LinearLayout)findViewById(R.id.timelayout);
+        rb1 =(RadioButton)findViewById(R.id.radioButton);
+        rb4 =(RadioButton)findViewById(R.id.radioButton4);
     }
 }
